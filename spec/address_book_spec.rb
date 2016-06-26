5ee025d7bd47104adda20816e307776cd50f89c8
Thuy Pham <tpham045@hotmail.com>
2016-06-26 20:12:11 +0000

    Refactor addres_book_spec

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -1,31 +1,28 @@
   require_relative '../models/address_book'
  
  RSpec.describe AddressBook do
+   let(:book) {AddressBook.new}
    describe "attributes" do
      it "responds to entries" do
-       book = AddressBook.new
        expect(book).to respond_to(:entries)
      end
      it "initializes entries as an array" do
-       book = AddressBook.new
        expect(book.entries).to be_an(Array)
      end
  
      it "initializes entries as empty" do
-       book = AddressBook.new
        expect(book.entries.size).to eq(0)
      end
    end
   describe "#add_entry" do
+    let(:book) {AddressBook.new}
      it "adds only one entry to the address book" do
-       book = AddressBook.new
        book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
  
        expect(book.entries.size).to eq(1)
      end
  
      it "adds the correct information to entries" do
-       book = AddressBook.new
        book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
        new_entry = book.entries[0]
  
